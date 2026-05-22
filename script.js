67178a2b9ac932735f3acfb83c477a7567e9da4e
ldonald1 <lelad12550@gmail.com>
2026-05-22 10:48:00 -0400

    Add functionality to calculator
    Added working operations (addition, subtraction,
    multiplication, division) that properly store and
    calculate results.

--- script.js
+++ script.js
@@ -17,16 +17,16 @@ function divide(arg1, arg2){
 function operate(op, arg1, arg2){
     switch (op){
         case '+':
-            add(arg1, arg2);
+            return add(arg1, arg2);
             break;
         case '-':
-            subtract(arg1, arg2);
+            return subtract(arg1, arg2);
             break;
         case '*':
-            multiply(arg1, arg2);
+            return multiply(arg1, arg2);
             break;
         case '/':
-            divide(arg1, arg2);
+            return divide(arg1, arg2);
             break;
     }
 }
@@ -37,24 +37,35 @@ const allButtons = document.querySelector(".allButtons");
 const clear = document.querySelector("#clear");
 
 let currentExpression = "";
+let currentOp = null;
+let currentArg1 = null;
+let currentArg2 = null;
 
 display.textContent = currentExpression;
 
 allButtons.addEventListener("click", (e) => {
     const button = e.target;
-    if(button.tagName === 'BUTTON' && !(button.classList.contains("equals")) && !(button.classList.contains("clear"))){
+    if(button.tagName === 'BUTTON' && !(button.classList.contains("equals")) && !(button.classList.contains("clear")) && !(button.parentElement.classList.contains("ops"))){
         currentExpression+=button.textContent;
         display.textContent = currentExpression;
+        console.log(`currentExpression: ${currentExpression}`);
     }
+    else if(button.tagName === 'BUTTON' && (button.parentElement.classList.contains("ops")) && !(button.classList.contains("clear"))){
+        currentOp = button.textContent;
+        currentArg1 = +currentExpression;
+        currentExpression = "";
+        console.log(`currentArg1: ${currentArg1}`);
+        }
     else if(button.tagName === 'BUTTON' && button.classList.contains("equals")){
-        /*
-        operate()
-        currentExpression+=button.textContent;
+        currentArg2 = +currentExpression;
+        console.log(`currentArg2: ${currentArg2}`);
+        currentExpression = operate(currentOp, currentArg1, currentArg2);
+        console.log(`Expression after operation: ${currentExpression}`);
         display.textContent = currentExpression;
-        */
     }
 
     else if(button.tagName === 'BUTTON' && button.classList.contains("clear")){
+        currentOp = null;
         currentExpression = "";
         display.textContent = currentExpression;
     }
