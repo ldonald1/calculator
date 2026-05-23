1484e6440efb70fbd49c57d71aefa2a4c502e937
ldonald1 <lelad12550@gmail.com>
2026-05-23 12:15:24 -0400

    Fix issues with sequential operations
    The calculator did not properly calculate pairs
    (i.e. (2+3)+5). Fixed code so that when 1st
    and 2nd arguments are entered, it calculates the
    result of the operation and then makes that
    result the new 1st argument. This allows for user
    to enter expressions such as '+5', and receive
    the correct answer.

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ function subtract(arg1, arg2){
 }
 
 function multiply(arg1, arg2){
-    return arg1 * arg2;
+    return +(arg1 * arg2).toFixed(12);
 }
 
 function divide(arg1, arg2){
-    return arg1 / arg2;
+    return +(arg1 / arg2).toFixed(12);
 }
 
 function operate(op, arg1, arg2){
@@ -45,27 +45,53 @@ display.textContent = currentExpression;
 
 allButtons.addEventListener("click", (e) => {
     const button = e.target;
+    // Display numbers on screen when you press the number buttons (and add to current expression)
     if(button.tagName === 'BUTTON' && !(button.classList.contains("equals")) && !(button.classList.contains("clear")) && !(button.parentElement.classList.contains("ops"))){
         currentExpression+=button.textContent;
         display.textContent = currentExpression;
-        console.log(`currentExpression: ${currentExpression}`);
     }
+    // Perform operations
     else if(button.tagName === 'BUTTON' && (button.parentElement.classList.contains("ops")) && !(button.classList.contains("clear"))){
         currentOp = button.textContent;
+       
+        if(!currentExpression){
+            return;
+        }
+        // Case for setting arg2 and performing op for arg1 and arg2
+        if(currentArg1 && !currentArg2){
+            
+            currentArg2 = +currentExpression;
+            currentExpression = operate(currentOp, currentArg1, currentArg2);
+            display.textContent = currentExpression;
+            currentArg1 = +currentExpression;
+            currentArg2 = null;
+            currentExpression = "";
+            return;
+        }
+        // Case for setting arg1 and 1st op
+        
         currentArg1 = +currentExpression;
         currentExpression = "";
-        console.log(`currentArg1: ${currentArg1}`);
         }
+
+        // Calculate result of operations upon pressing equals button
     else if(button.tagName === 'BUTTON' && button.classList.contains("equals")){
         currentArg2 = +currentExpression;
-        console.log(`currentArg2: ${currentArg2}`);
+       
         currentExpression = operate(currentOp, currentArg1, currentArg2);
-        console.log(`Expression after operation: ${currentExpression}`);
+        currentArg1 = +currentExpression;
         display.textContent = currentExpression;
+        currentExpression = "";
+        currentOp = null;
+        
+        currentArg2 = null;
     }
 
+    // Clear all data from calculator upon pressing clear button
     else if(button.tagName === 'BUTTON' && button.classList.contains("clear")){
         currentOp = null;
+        currentArg1 = null;
+        currentArg2 = null;
         currentExpression = "";
         display.textContent = currentExpression;
     }
