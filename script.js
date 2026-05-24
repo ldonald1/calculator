bbc9425f16e325f89fc2ce6501e44ffe83d269ca
ldonald1 <lelad12550@gmail.com>
2026-05-23 23:26:37 -0400

    Fix calculation errors
    Fixed errors where operations could not be
    performed on result of the equals button, and
    where entering number after equals did not allow
    expressions to be computed properly.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function add(arg1, arg2){
-    return arg1 + arg2;
+    return +(arg1 + arg2).toFixed(12);
 }
 
 function subtract(arg1, arg2){
-    return arg1 - arg2;
+    return +(arg1 - arg2).toFixed(12);
 }
 
 function multiply(arg1, arg2){
@@ -11,6 +11,9 @@ function multiply(arg1, arg2){
 }
 
 function divide(arg1, arg2){
+    if(arg2 === 0){
+        return "ERR: DIV BY ZERO";
+    }
     return +(arg1 / arg2).toFixed(12);
 }
 
@@ -40,6 +43,7 @@ let currentExpression = "";
 let currentOp = null;
 let currentArg1 = null;
 let currentArg2 = null;
+let arg1AfterCalculation = false;
 
 display.textContent = currentExpression;
 
@@ -47,44 +51,67 @@ allButtons.addEventListener("click", (e) => {
     const button = e.target;
     // Display numbers on screen when you press the number buttons (and add to current expression)
     if(button.tagName === 'BUTTON' && !(button.classList.contains("equals")) && !(button.classList.contains("clear")) && !(button.parentElement.classList.contains("ops"))){
+        if(arg1AfterCalculation && currentOp === null){
+            currentArg1 = null;
+        }
         currentExpression+=button.textContent;
         display.textContent = currentExpression;
     }
     // Perform operations
     else if(button.tagName === 'BUTTON' && (button.parentElement.classList.contains("ops")) && !(button.classList.contains("clear"))){
-        currentOp = button.textContent;
+
        
-        if(!currentExpression){
-            return;
-        }
         // Case for setting arg2 and performing op for arg1 and arg2
         if(currentArg1 && !currentArg2){
             
+
+            if(currentExpression === ""){
+                currentExpression = currentArg1;
+            }
+
+            else{
             currentArg2 = +currentExpression;
             currentExpression = operate(currentOp, currentArg1, currentArg2);
+            if(currentExpression === "ERR: DIV BY ZERO"){
+                currentExpression = "";
+                currentArg1 = null;
+                currentOp = null;
+                currentArg2 = null;
+                return;
+                }
+            }
+            currentOp = button.textContent;
             display.textContent = currentExpression;
             currentArg1 = +currentExpression;
             currentArg2 = null;
             currentExpression = "";
             return;
         }
+
         // Case for setting arg1 and 1st op
-        
+        currentOp = button.textContent;
         currentArg1 = +currentExpression;
         currentExpression = "";
         }
 
         // Calculate result of operations upon pressing equals button
     else if(button.tagName === 'BUTTON' && button.classList.contains("equals")){
+        if(currentExpression === ""){
+            display.textContent = 'ERROR';
+            currentArg1 = null;
+            currentOp = null;
+            currentArg2 = null;
+            return;
+        }
         currentArg2 = +currentExpression;
-       
         currentExpression = operate(currentOp, currentArg1, currentArg2);
-        currentArg1 = +currentExpression;
+        currentArg1 = currentExpression;
         display.textContent = currentExpression;
+
         currentExpression = "";
         currentOp = null;
-        
         currentArg2 = null;
+        arg1AfterCalculation = true;
     }
 
     // Clear all data from calculator upon pressing clear button
